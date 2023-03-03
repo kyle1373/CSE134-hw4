1567d043eafc5f717558f2eda0463e05bb3b0aca
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-03-02 23:30:42 -0800

    Made custom dialog modules

--- customdialog.js
+++ customdialog.js
@@ -1,39 +1,37 @@
-this.document.getElementById("showAlert").addEventListener("click", () => {
+const showAlert = () => {
   document.getElementById("alertDialog").showModal();
-});
+};
 
-this.document.getElementById("showConfirm").addEventListener("click", () => {
+const showConfirm = () => {
   document.getElementById("confirmDialog").showModal();
-});
+};
 
-this.document.getElementById("showName").addEventListener("click", () => {
+const showName = () => {
   document.getElementById("nameDialog").showModal()
-});
+};
 
-this.document.getElementById("okConfirm").addEventListener("click", () => {
+const okConfirm = () => {
   document.getElementById("outputPrompt").innerText = "Confirm result: true";
   document.getElementById("outputPrompt").hidden = false;
-});
+};
 
-this.document.getElementById("cancelConfirm").addEventListener("click", () => {
+const cancelConfirm = () => {
     document.getElementById("outputPrompt").innerText = "Confirm result: false";
     document.getElementById("outputPrompt").hidden = false;
-});
+};
 
-this.document.getElementById("okName").addEventListener("click", () => {
+const okName = () => {
   document.getElementById("nameDialog").close();
   let text = document.getElementById("nameInput").value;
   let outputPrompt = document.getElementById("outputPrompt");
   outputPrompt.innerText = "Prompt result: " + text;
   outputPrompt.hidden = false;
-});
+};
 
-this.document
-  .getElementById("cancelName")
-  .addEventListener("click", () => {
+const cancelName = () => {
     document.getElementById("nameDialog").close();
     let text = document.getElementById("inputtedName").value;
     let outputPrompt = document.getElementById("outputPrompt");
     outputPrompt.innerText = "No name provided";
     outputPrompt.hidden = false;
-  });
+  };
